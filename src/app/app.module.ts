43b8e93026fabf8adb7cbd93ac2c5910470f9b7c
sagadavid <epostsaga@gmail.com>
2023-07-03 11:37:38 +0200

    auth0 import to service.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { AuthModule } from '@auth0/auth0-angular';
+// import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // Import the module into the application, with configuration
-    AuthModule.forRoot({
-      domain: 'dev-sbuzcsubxvefrita.us.auth0.com',
-      clientId: 'ooh1vWjdGosg3MEcLrN3QshzO3y0QaeW',
-      authorizationParams: {
-        redirect_uri: window.location.origin,
-      },
-    }),
+    //// Import the module into the application, with configuration
+    // AuthModule.forRoot({
+    //   domain: 'dev-sbuzcsubxvefrita.us.auth0.com',
+    //   clientId: 'ooh1vWjdGosg3MEcLrN3QshzO3y0QaeW',
+    //   authorizationParams: {
+    //     redirect_uri: window.location.origin,
+    //   },
+    // }),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import * as auth0 from 'auth0-js';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  private _idToken: string;
+  private _accessToken: string;
+  private _expiresAt: number;
+
+  auth0 = new auth0.WebAuth({
+    clientID: '[your-client-id]',
+    domain: '[your-domain]',
+    responseType: 'token id_token',
+    redirectUri: 'http://localhost:4200/callback',
+    scope: 'openid',
+  });
+
+  constructor(public router: Router) {
+    this._idToken = '';
+    this._accessToken = '';
+    this._expiresAt = 0;
+  }
+
+  get accessToken(): string {
+    return this._accessToken;
+  }
+
+  get idToken(): string {
+    return this._idToken;
+  }
+
+  public login(): void {
+    this.auth0.authorize();
+  }
 }
